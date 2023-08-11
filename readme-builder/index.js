88fae531ac523af0352736994c154facf8e52c54
free-bots <free-bots.to@protonmail.com>
2023-08-11 22:58:37 +0200

    changed template url

--- readme-builder/index.js
+++ readme-builder/index.js
@@ -16,12 +16,13 @@ const __dirname = path.dirname(__filename);
     await page.setViewport({ width: 800, height: 1000, deviceScaleFactor: 2 });
     const url = join(__dirname, 'template', 'index.html');
     console.log(`Index.html is located at ${url}`);
-    await page.goto(url);
+    await page.goto(`file://${url}`);
     await page.screenshot({
         optimizeForSpeed: false,
         quality: 100,
         type: 'webp',
         path: join(__dirname, '..', 'readme.webp')
-    })
+    });
+    console.log('Created readme image');
     await browser.close();
 })();
