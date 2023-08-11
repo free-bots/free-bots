5a8d90a05a3878c8ddce036d24ca19222d7ff559
free-bots <free-bots.to@protonmail.com>
2023-08-11 22:29:20 +0200

    updated metrics workflow

--- readme-builder/index.js
+++ readme-builder/index.js
@@ -4,10 +4,19 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 (async () => {
-    const browser = await puppeteer.launch({ headless: 'new' });
+    const browser = await puppeteer.launch({
+        headless: 'new',
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+        ]
+    });
     const page = await browser.newPage();
     await page.setViewport({ width: 800, height: 1000, deviceScaleFactor: 2 });
-    await page.goto(join(__dirname, 'template', "index.html"));
+    const url = join(__dirname, 'template', 'index.html');
+    console.log(`Index.html is located at ${url}`);
+    await page.goto(url);
     await page.screenshot({
         optimizeForSpeed: false,
         quality: 100,
