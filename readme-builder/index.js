491b1d55e8861dd827b4607b57d1be38de040051
free-bots <free-bots.to@protonmail.com>
2025-08-09 13:49:26 +0200

    changed design

--- readme-builder/index.js
+++ readme-builder/index.js
@@ -1,10 +1,29 @@
 import puppeteer from 'puppeteer';
+import Handlebars from "handlebars";
 import fs from 'fs';
 import path, { join } from 'path';
 import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 (async () => {
+    const loadFileAsString = (path) => fs.readFileSync(path).toString();
+
+    const achievements = loadFileAsString(join(__dirname, '..', 'metrics.achievements.svg'));
+    const activity = loadFileAsString(join(__dirname, '..', 'metrics.activity.svg'));
+    const community = loadFileAsString(join(__dirname, '..', 'metrics.community.svg'));
+    const languages = loadFileAsString(join(__dirname, '..', 'metrics.languages.svg'));
+
+    const templlateFile =  fs.readFileSync(join(__dirname, 'template', 'index.handlebars'));
+    const template = Handlebars.compile(templlateFile.toString());
+    const rendered = template({ 
+        achievements,
+        activity,
+        community,
+        languages
+    }); 
+
+    fs.writeFileSync(join(__dirname, 'template', 'index.html'), rendered);
+
     const browser = await puppeteer.launch({
         headless: 'new',
         args: [
@@ -14,10 +33,14 @@ const __dirname = path.dirname(__filename);
         ]
     });
     const page = await browser.newPage();
-    await page.setViewport({ width: 800, height: 1200, deviceScaleFactor: 2 });
     const url = join(__dirname, 'template', 'index.html');
     console.log(`Index.html is located at ${url}`);
     await page.goto(`file://${url}`);
+
+    const bottom = await page.evaluate('document.getElementById("end").getBoundingClientRect().bottom');
+    const height = Math.round(bottom + 20);
+    await page.setViewport({ width: 800, height: height, deviceScaleFactor: 2 });
+
     const readmeImagePath = join(__dirname, '..', 'readme.webp');
     await page.screenshot({
         optimizeForSpeed: false,
